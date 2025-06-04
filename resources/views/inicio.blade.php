72255fe07a4b6c0762f7ce3dd68e6542927b557a
Puchungu <nelsonalvarenga1000@gmail.com>
2025-06-04 16:45:44 -0600

    Add CSS classes to headings and paragraphs in inicio.blade.php for consistent styling

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     @auth
-    <h1>Bienvenido, {{ Auth::user()->username }}</h1>
+    <h1 class="titulo-principal">Bienvenido, {{ Auth::user()->username }}</h1>
     <form action="{{ route('logout') }}" method="POST">
         @csrf
         <button type="submit">Cerrar sesión</button>
@@ -15,10 +15,10 @@
     @endauth
 
     @guest
-    <h1>Bienvenido a CanScan</h1>
-    <p>CanScan es una plataforma para que tu vida sea mas saludable y puedas saber que contienen los productos que consumes.</p>
-    <p>Lamentablemente aun estamos en desarrollo, proximamente podras empezar a ser mas saludable</p>
-    <p>Por favor, <a href="{{ route('mostrar.Login') }}">inicia sesión</a> o <a href="{{ route('mostrar.Registro') }}">regístrate</a> para continuar.</p>
+    <h1 class="titulo-principal">Bienvenido a CanScan</h1>
+    <p class="parrafo">CanScan es una plataforma para que tu vida sea mas saludable y puedas saber que contienen los productos que consumes.</p>
+    <p class="parrafo">Lamentablemente aun estamos en desarrollo, proximamente podras empezar a vivir una vida mas saludable</p>
+    <p class="parrafo">Por favor, <a href="{{ route('mostrar.Login') }}">inicia sesión</a> o <a href="{{ route('mostrar.Registro') }}">regístrate</a> para continuar.</p>
     @endguest
 </body>
 </html>
