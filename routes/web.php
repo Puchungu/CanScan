3cd1f393017c46ce14012d14d3d78ae5146145a6
Puchungu <nelsonalvarenga1000@gmail.com>
2025-06-05 21:34:59 -0600

    Fix email verification route to return the correct view and clean up code structure

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest; 
+use Illuminate\Http\Request;
 
 Route::get('/',[AuthController::class, 'mostrarInicio'])->name('mostrar.Inicio');
 
 Route::get('/registrarse', [AuthController::class, 'mostrarRegistro'])->name('mostrar.Registro');
 Route::post('/registrarse', [AuthController::class, 'Registrar'])->name('Registrarse');
 
-Route::get('/login', [AuthController::class, 'mostrarInicioSesion'])->name('mostrar.Login');
 Route::post('/login', [AuthController::class, 'iniciarSesion'])->name('iniciarSesion');
 Route::get('/login', [AuthController::class, 'mostrarInicioSesion'])->name('login');
 
 Route::post('/logout', [AuthController::class, 'cerrarSesion'])->name('logout');
 
 Route::get('/email/verify', function () {
-    return view('login');
+    return view('emailver');
 })->middleware('auth')->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
@@ -24,3 +24,9 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
  
     return redirect('/');
 })->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Correo de verificación reenviado.');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
