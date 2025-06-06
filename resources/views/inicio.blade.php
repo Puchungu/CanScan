f8ff7bbbcc7548af72c528634c916a02372db741
Puchungu <nelsonalvarenga1000@gmail.com>
2025-06-05 21:34:48 -0600

    Fix route reference for login link in inicio.blade.php

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -18,7 +18,7 @@
     <h1 class="titulo-principal">Bienvenido a CanScan</h1>
     <p class="parrafo">CanScan es una plataforma para que tu vida sea mas saludable y puedas saber que contienen los productos que consumes.</p>
     <p class="parrafo">Lamentablemente aun estamos en desarrollo, proximamente podras empezar a vivir una vida mas saludable</p>
-    <p class="parrafo">Por favor, <a href="{{ route('mostrar.Login') }}">inicia sesión</a> o <a href="{{ route('mostrar.Registro') }}">regístrate</a> para continuar.</p>
+    <p class="parrafo">Por favor, <a href="{{ route('login') }}">inicia sesión</a> o <a href="{{ route('mostrar.Registro') }}">regístrate</a> para continuar.</p>
     @endguest
 </body>
 </html>
