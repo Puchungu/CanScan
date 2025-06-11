977139322ebf0b54384b2cc32fc95762048d278b
Puchungu <nelsonalvarenga1000@gmail.com>
2025-06-11 15:12:03 -0600

    Update productos table to change image column type from binary to string and enhance product view to display component descriptions

--- resources/views/producto.blade.php
+++ resources/views/producto.blade.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <title>Códigos de Barras Escaneados</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Códigos de Barras Escaneados</h1>
     @foreach ($producto as $producto)
-        <div>
-            <h2>{{ $producto->nombre }}</h2>
-            <p>Código de barras: {{ $producto->codigo_barra }}</p>
-            <h3>Componentes:</h3>
-            <ul>
-                @foreach ($producto->componentes as $componente)
-                    <li>{{ $componente->nombre }}</li>
-                @endforeach
-            </ul>
-        </div>
+        <img src="{{ asset($producto->img) }}" alt="{{$producto->nombre}}" class="img-fluid mb-3">
+        <h2 class="card-title mb-3">{{ $producto->nombre }}</h2>
+        <h5 class="mb-3 text-secondary">Componentes:</h5>
+        @foreach ($producto->componentes as $componente)
+            <div class="border rounded p-2 bg-light">
+                <span class="d-block">{{ $componente->nombre }}</span>
+                <span> {{$componente->descripcion}}</span>
+            </div>
+        @endforeach
     @endforeach
 </body>
 </html>
